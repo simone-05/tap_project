f6e0dd27c85e05d68229bc9f5f9513d87e764020
Simone <73489163+simone-05@users.noreply.github.com>
2022-06-27 23:30:35 +0200

    fixed get-spark.sh not downloading archive because of certificates errors

--- spark/get-spark.sh
+++ spark/get-spark.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O spark/spark-3.2.1-bin-hadoop3.2.tgz https://dlcdn.apache.org/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
+wget --no-check-certificate -O spark/spark-3.2.1-bin-hadoop3.2.tgz https://dlcdn.apache.org/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
