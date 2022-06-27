f620598c769cdd1cda11af109957232d3d0df267
Simone <73489163+simone-05@users.noreply.github.com>
2022-06-28 00:19:08 +0200

    updated start.sh with skipping spark archive

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-printf "\n-- Deleting spark archive --\n"
-rm spark/*.tgz
-printf "\n-- Downloading spark archive --\n"
-spark/get-spark.sh
+
+first_argument="skip"
+
+if [[ $1 = $first_argument ]]; then
+    printf "\n-- Skipping downloading spark archive --\n"
+else
+    printf "\n-- Deleting spark archive --\n"
+    rm spark/*.tgz
+    printf "\n-- Downloading spark archive --\n"
+    spark/get-spark.sh
+fi
+
 
 # Optional:
 # printf "\n-- Updating songs trainging set --\n"
